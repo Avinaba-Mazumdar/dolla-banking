c36ec53373b7300be0502a2660865de07cc4e0f8
Avinaba-Mazumdar <avinaba2@gmail.com>
2021-07-17 23:17:21 +0530

    InfoSection data

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,102 @@
+export const homeObjOne = {
+      id: 'about',
+      lightBg: false,
+      lightText: true,
+      lightTextDesc: true,
+      topLine: 'Premium Bank',
+      headline: 'Unlimited Transactions with Zero Fees',
+      description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+      buttonLabel: 'Get Started',
+      imgStart: false,
+      img: '../../media/svg1.svg',
+      alt: 'Car',
+      dark: true,
+      dark2: true,
+      primary: true,
+      darkText: false
+};
+
+export const homeObjTwo = {
+      id: 'discover',
+      lightBg: true,
+      lightText: false,
+      lighttextDesc: false,
+      topLine: 'Unlimited Access',
+      headline: 'Login to your account at any time',
+      description: 'Get access to our exclusive app that allows you to send unlimited transactions whitout getting charged any fees.',
+      buttonLabel: 'Learn More',
+      imgStart: true,
+      img: '../../media/svg2.svg',
+      alt: 'Piggybank',
+      dark: false,
+      primary: false,
+      darkText: true
+};
+    
+export const homeObjThree = {
+      id: 'signup',
+      lightBg: true,
+      lightText: false,
+      lighttextDesc: false,
+      topLine: 'Join Our Team',
+      headline: 'Creating an account is extremely easy',
+      description: "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+      buttonLabel: 'Start Now',
+      imgStart: false,
+      img: '../../media/svg3.svg',
+      alt: 'Paper',
+      dark: false,
+      primary: false,
+      darkText: true
+};
+
+export const homeObjFour = {
+      id: 'signup',
+      lightBg: true,
+      lightText: false,
+      lighttextDesc: false,
+      topLine: 'Join Our Team',
+      headline: 'Creating an account is extremely easy',
+      description: "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+      buttonLabel: 'Start Now',
+      imgStart: false,
+      img: '../../media/svg2.svg',
+      alt: 'Paper',
+      dark: false,
+      primary: false,
+      darkText: true
+};
+
+export const homeObjFive = {
+      id: 'signup',
+      lightBg: true,
+      lightText: false,
+      lighttextDesc: false,
+      topLine: 'Join Our Team',
+      headline: 'Creating an account is extremely easy',
+      description: "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+      buttonLabel: 'Start Now',
+      imgStart: false,
+      img: '../../media/svg3.svg',
+      alt: 'Paper',
+      dark: false,
+      primary: false,
+      darkText: true
+};
+
+export const homeObjSix = {
+      id: 'signup',
+      lightBg: true,
+      lightText: false,
+      lighttextDesc: false,
+      topLine: 'Join Our Team',
+      headline: 'Creating an account is extremely easy',
+      description: "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+      buttonLabel: 'Start Now',
+      imgStart: false,
+      img: '/media/svg-4.svg',
+      alt: 'Paper',
+      dark: false,
+      primary: false,
+      darkText: true
+};
